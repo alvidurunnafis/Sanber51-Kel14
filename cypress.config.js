2f6698f5030f013adaf06e4ee89db2f1b83cd736
lisallah20 <lisallah002@gmail.com>
2023-11-26 23:58:12 +0700

    Add Choose Product Testing and Update File Config

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    defaultCommandTimeout: 8000,
+    defaultCommandTimeout: 15000,
     viewportHeight: 750
   },
 });
---
+++ cypress/e2e/3-magento/cproduct.cy.js
@@ -0,0 +1,64 @@
+let data = require('../../fixtures/userData.json')
+
+describe('Verify Choose Product Functionality', () => {
+    it('Success Add One Item', () => {
+      cy.goto('https://magento.softwaretestingboard.com/') // CUSTOM COMMAND
+      cy.get('.panel > .header > .authorization-link').click()
+      cy.get('#email').type(data.validData.validUser)
+      cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type(data.validData.validPass)
+      cy.get('.action.login.primary').click()
+      cy.get(':nth-child(5) > .product-item-info > .product-item-photo > .product-image-container > .product-image-wrapper > .product-image-photo').click()
+      cy.get('#product-addtocart-button').click()
+    })
+    it('Success Add 15 Same Item', () => {
+        cy.goto('https://magento.softwaretestingboard.com/') // CUSTOM COMMAND
+        cy.get('.panel > .header > .authorization-link').click()
+        cy.get('#email').type(data.validData.validUser)
+        cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type(data.validData.validPass)
+        cy.get('.action.login.primary').click()
+        cy.get(':nth-child(5) > .product-item-info > .product-item-photo > .product-image-container > .product-image-wrapper > .product-image-photo').click()
+        cy.get('#qty').type('5')
+        cy.get('#product-addtocart-button').click()
+      })
+      it('Success/Failed Add Wishlist Item', () => {
+        cy.goto('https://magento.softwaretestingboard.com/') // CUSTOM COMMAND
+        cy.get('.panel > .header > .authorization-link').click()
+        cy.get('#email').type(data.validData.validUser)
+        cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type(data.validData.validPass)
+        cy.get('.action.login.primary').click()
+        cy.get(':nth-child(5) > .product-item-info > .product-item-photo > .product-image-container > .product-image-wrapper > .product-image-photo').click()
+        cy.get('.action.towishlist').click()
+        if ('.page.message.message-error') {
+            cy.get('.message-error > div').should('contain', 'Invalid Form Key')
+        } else {
+            cy.get('.message-success > div').should('contain', 'You added')
+        }
+      }) 
+      it('Failed/Success Add to Comparing Item - Invalid Form Key', () => {
+        cy.goto('https://magento.softwaretestingboard.com/') // CUSTOM COMMAND
+        cy.get('.panel > .header > .authorization-link').click()
+        cy.get('#email').type(data.validData.validUser)
+        cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type(data.validData.validPass)
+        cy.get('.action.login.primary').click()
+        cy.get(':nth-child(5) > .product-item-info > .product-item-photo > .product-image-container > .product-image-wrapper > .product-image-photo').click()
+        cy.get('.action.tocompare').click()
+        if ('.page.message.message-error') {
+            cy.get('.message-error > div').should('contain', 'Invalid Form Key')
+        } else {
+            cy.get('.message-success > div').should('contain', 'You added')
+        }
+      })
+      it.only('Success Choose Other Product in Detail Product Page', () => {
+        cy.goto('https://magento.softwaretestingboard.com/') // CUSTOM COMMAND
+        cy.get('.panel > .header > .authorization-link').click()
+        cy.get('#email').type(data.validData.validUser)
+        cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type(data.validData.validPass)
+        cy.get('.action.login.primary').click()
+        cy.get(':nth-child(5) > .product-item-info > .product-item-photo > .product-image-container > .product-image-wrapper > .product-image-photo').click()
+        cy.get('.product-item-link').click()
+        cy.get('#product-addtocart-button').click()
+      })
+})
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+  })
